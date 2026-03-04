0715f77ab4891c15b210c361b6d4f8e464e2cf32
Tectors <73559984+Tectors@users.noreply.github.com>
2026-03-04 13:36:59 -0800

    Update icons, and onboarding

--- Source/vj0/Views/Onboarding/OnboardingWelcomeView.cs
+++ Source/vj0/Views/Onboarding/OnboardingWelcomeView.cs
@@ -1,6 +1,9 @@
+using System.Threading.Tasks;
+using Avalonia.Animation.Easings;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
-
+using Avalonia.Media;
 using vj0.Windows;
 
 namespace vj0.Views.Onboarding;
@@ -16,5 +19,60 @@ public partial class OnboardingWelcomeView : UserControl
     {
         var window = VisualRoot as OnboardingWindow;
         window!.GoNext();
+    }
+
+    private bool IsLogoAnimationPlaying;
+    
+    private async void PlayLogoAnimation()
+    {
+        if (IsLogoAnimationPlaying) return;
+        
+        if (Logo?.RenderTransform is not ScaleTransform Transform)
+        {
+            return;
+        }
+
+        const double startScale = 1.0;
+        const double endScale = 1.5;
+        const double durationMs = 30;
+        const int stepMs = 1;
+
+        Logo.Opacity = 1.0;
+
+        var startOpacity = Logo.Opacity;
+        const double endOpacity = 0.0;
+
+        double elapsed = 0;
+        
+        IsLogoAnimationPlaying = true;
+
+        while (elapsed < durationMs)
+        {
+            var time = elapsed / durationMs;
+            var eased = new CubicEaseOut().Ease(time);
+
+            var currentScale = startScale + (endScale - startScale) * eased;
+            var currentOpacity = startOpacity + (endOpacity - startOpacity) * eased;
+
+            Transform.ScaleX = currentScale;
+            Transform.ScaleY = currentScale;
+            Logo.Opacity = currentOpacity;
+
+            await Task.Delay(stepMs);
+            
+            elapsed += stepMs;
+        }
+
+        Transform.ScaleX = 1.0;
+        Transform.ScaleY = 1.0;
+        
+        Logo.Opacity = 0.0;
+        
+        IsLogoAnimationPlaying = false;
+    }
+
+    private void InputElement_OnPointerEntered(object? sender, PointerEventArgs e)
+    {
+        PlayLogoAnimation();
     }
 }
