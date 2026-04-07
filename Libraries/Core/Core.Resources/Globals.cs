90aba211de63913f8fa1baf3ec30325332b61765
Tectors <73559984+Tectors@users.noreply.github.com>
2026-04-06 20:15:15 -0700

    0.0.5.0

--- Libraries/Core/Core.Resources/Globals.cs
+++ Libraries/Core/Core.Resources/Globals.cs
@@ -8,7 +8,7 @@ namespace Core.Resources;
 public static class Globals
 {
     /* Format: 0.0.0.0 */
-    public const string VERSION = "0.0.4.9";
+    public const string VERSION = "0.0.5.0";
     public const string COMMIT = "";
     
     public static bool IS_COMMIT_AVAILABLE => !string.IsNullOrEmpty(COMMIT);
